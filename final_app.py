06dc80c09102abf35c757d7be3bca852a19b0275
katemathew <65739191+katemathew@users.noreply.github.com>
2024-04-30 15:17:21 -0700

    Update final_app.py

--- final_app.py
+++ final_app.py
@@ -133,7 +133,10 @@ def load_filtered_spotify_data():
     return df
 
 def load_spotify_tracks_db():
-    database_url = os.getenv("DATABASE_URL")
+    # Hardcoded database URL
+    database_url = "postgresql://u4ja2bod19v7gd:p9e70065bd97ea89a78fd91429d857f1c6dcb32c248a847c624d3a359bdeba876@ce1r1ldap2qd4b.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/db3gjtci88doqv"
+
+    # Database connection setup
     engine = create_engine(database_url)
     Session = sessionmaker(bind=engine)
     session = Session()
