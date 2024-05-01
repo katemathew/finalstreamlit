febe33538e989d271b1f20c181dbe1e103f33297
katemathew <65739191+katemathew@users.noreply.github.com>
2024-05-01 01:57:05 -0700

    Update datasetdescription.py

--- pages/datasetdescription.py
+++ pages/datasetdescription.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+def app():
+    st.title('Dataset Description')
+    st.write("""
+    Finally, you should have a tab that’s basically a description of your datasets. This should be similar to what you submitted for Milestone 1.
+    """)
+
+if __name__ == "__main__":
+    app()
 
