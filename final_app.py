f66fb2cb0509ebdb542fe2cfef1939efd1dab8de
katemathew <65739191+katemathew@users.noreply.github.com>
2024-05-01 19:36:53 -0700

    Update final_app.py

--- final_app.py
+++ final_app.py
@@ -417,6 +417,9 @@ def main():
     artist_list = tracks['Artist'].unique()
     selected_artist = st.selectbox('**Select an Artist**', artist_list)
 
+    # Note below the dropdown
+    st.caption("Different artists may not have the same results, some may have a lot of data, whereas others may have fewer entries or none. This really depends on if their performance data is available on Setlist.FM, and correlates to data in both the Kaggle dataset as well as the Spotify API.")
+
     # Filter data by selected artist
     filtered_setlist_data = setlist_data[setlist_data['Artist'] == selected_artist]
     filtered_spotify_data = spotify_data[spotify_data['Artist'] == selected_artist]
