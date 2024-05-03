72d8ad2f14faad526e2d983e0ef51fdc064670eb
katemathew <65739191+katemathew@users.noreply.github.com>
2024-05-03 04:19:55 -0700

    test

--- final_app.py
+++ final_app.py
@@ -500,7 +500,7 @@ def main():
     filtered_setlist_data = filtered_setlist_data.drop(columns='Artist')
 
     filtered_spotify_data = spotify_data[spotify_data['Artist'] == selected_artist]
-    filtered_spotify_data = filtered_spotify_data.drop_duplicates(subset='name', keep='first').drop(columns=['duration_ms', 'popularity', 'id', 'artists', 'Artist'])
+    #filtered_spotify_data = filtered_spotify_data.drop_duplicates(subset='name', keep='first').drop(columns=['duration_ms', 'popularity', 'id', 'artists', 'Artist'])
 
     filtered_tracks = tracks[tracks['Artist'] == selected_artist]
     filtered_tracks = filtered_tracks.drop(columns='track_id')
