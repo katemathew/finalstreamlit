2131ba57c44c172646e4f378967a1fce7616b6b3
katemathew <65739191+katemathew@users.noreply.github.com>
2024-05-03 01:13:00 -0700

    Update final_app.py

--- final_app.py
+++ final_app.py
@@ -200,10 +200,10 @@ def load_spotify_tracks_db():
 #     final_combined_data = pd.merge(combined_data, df3, on=key, how='inner')
 #     return final_combined_data
 
-# def analyze_overlaps(df1, df2, df3, key='name'):
-#     combined_data = pd.merge(df1, df2, on=key, how='inner')
-#     final_combined_data = pd.merge(combined_data, df3, on=key, how='inner')
-#     return final_combined_data
+def analyze_overlaps(df1, df2, df3, key='name'):
+    combined_data = pd.merge(df1, df2, on=key, how='inner')
+    final_combined_data = pd.merge(combined_data, df3, on=key, how='inner')
+    return final_combined_data
 
 def plot_data(df):
     if df.empty:
