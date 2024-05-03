b499486117920bda04f6e4fd0b7ba47cf5af3135
katemathew <65739191+katemathew@users.noreply.github.com>
2024-05-03 02:42:39 -0700

    cleaning dataframes more

--- final_app.py
+++ final_app.py
@@ -195,7 +195,7 @@ def load_spotify_tracks_db():
 
     return tracks
 
-def analyze_overlaps(df1, df2, df3, key='Artist'):
+def analyze_overlaps(df1, df2, df3, key='name'):
     combined_data = pd.merge(df1, df2, on=key, how='inner')
     final_combined_data = pd.merge(combined_data, df3, on=key, how='inner')
     return final_combined_data
@@ -494,8 +494,14 @@ def main():
 
     # Filter data by selected artist
     filtered_setlist_data = setlist_data[setlist_data['Artist'] == selected_artist]
+    filtered_setlist_data = filtered_setlist_data.drop(columns='Artist')
+
     filtered_spotify_data = spotify_data[spotify_data['Artist'] == selected_artist]
+    filtered_spotify_data = filtered_spotify_data.drop_duplicates(subset='name', keep='first').drop(columns=['duration_ms', 'popularity', 'id', 'artists', 'Artist'])
+
     filtered_tracks = tracks[tracks['Artist'] == selected_artist]
+    filtered_tracks = filtered_tracks.drop(columns='track_id')
+
     st.header('setlist')
     st.write(filtered_setlist_data.head(15))
     st.header('kaggle')
