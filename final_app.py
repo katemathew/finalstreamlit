00e4191b0410c49124c414bb9a203c27fddade98
katemathew <65739191+katemathew@users.noreply.github.com>
2024-05-03 02:50:36 -0700

    trying outer join

--- final_app.py
+++ final_app.py
@@ -196,8 +196,8 @@ def load_spotify_tracks_db():
     return tracks
 
 def analyze_overlaps(df1, df2, df3, key):
-    combined_data = pd.merge(df1, df2, on=key, how='inner')
-    final_combined_data = pd.merge(combined_data, df3, on=key, how='inner')
+    combined_data = pd.merge(df1, df2, on=key, how='outer')
+    final_combined_data = pd.merge(combined_data, df3, on=key, how='outer')
     return final_combined_data
 
 
